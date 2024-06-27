65ad3e0179f75f7fc14d8cf300a491e71119ca1f
Exylerr <148752815+Exylerr@users.noreply.github.com>
2024-06-27 22:36:28 +0800

    forgot password all done

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrl: './forgot-password.component.css'
+  styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent {
 
+  forgotpasswordEmail: string = '';
+  emailValid: boolean = true; // Flag to track email validation status
+
+  constructor(private router: Router) { }
+
+  onCloseButtonClick(): void {
+    this.router.navigate(['/loginPage']);
+  }
+
+  emailValidation(event: string) {
+    const value = event.trim(); // Trim whitespace from input
+    const emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+
+    // Perform validation
+    if (value) {
+      this.emailValid = emailPattern.test(value); // Validate email format
+    } else {
+      this.emailValid = false; // Empty input is invalid
+    }
+
+    this.forgotpasswordEmail = value; // Update bound variable with trimmed value
+  }
 }
